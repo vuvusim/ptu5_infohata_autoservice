bc3010d2220aa02d092b4f169dd9a88e6e393720
simas venskus <simasvenskus@gmail.com>
2022-11-15 22:07:03 +0200

    initial admin improvements

--- ptu5_autoservice/autoservice/admin.py
+++ ptu5_autoservice/autoservice/admin.py
@@ -16,10 +16,22 @@ class OrderLineInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineInline,)
     list_filter = ('date', 'status')
+    list_display = ('id', 'date', 'total', 'car')
 
 
-admin.site.register(models.Car)
+class CarAdmin(admin.ModelAdmin):
+    list_filter = ('client', 'car_model')
+    list_display = ('client', 'car_model', 'plate', 'vin')
+    search_fields = ('vin', 'plate')
+    
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')     
+
+
+admin.site.register(models.Car, CarAdmin)
 admin.site.register(models.CarModel)
-admin.site.register(models.Service)
+admin.site.register(models.Service, ServiceAdmin)
 admin.site.register(models.Order, OrderAdmin)
 admin.site.register(models.OrderLine, OrderLineAdmin)
