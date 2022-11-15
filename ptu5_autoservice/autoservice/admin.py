53ddf116b581bb942d24a03005a97a8a3e5fbf92
simas venskus <simasvenskus@gmail.com>
2022-11-15 21:29:37 +0200

    order line inlines in admin

--- ptu5_autoservice/autoservice/admin.py
+++ ptu5_autoservice/autoservice/admin.py
@@ -8,8 +8,18 @@ class OrderLineAdmin(admin.ModelAdmin):
     list_filter = ('order', )
 
 
+class OrderLineInline(admin.TabularInline):
+    model = models.OrderLine
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineInline,)
+    list_filter = ('date', 'status')
+
+
 admin.site.register(models.Car)
 admin.site.register(models.CarModel)
 admin.site.register(models.Service)
-admin.site.register(models.Order)
+admin.site.register(models.Order, OrderAdmin)
 admin.site.register(models.OrderLine, OrderLineAdmin)
