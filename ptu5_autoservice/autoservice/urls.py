bb52bac161f1f9d531d4f981402ab7b4b7797e09
simas venskus <simasvenskus@gmail.com>
2022-11-16 23:00:38 +0200

    class based order list and detail views

--- ptu5_autoservice/autoservice/urls.py
+++ ptu5_autoservice/autoservice/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('cars/', views.car_list_view, name='cars'),
     path('car/<int:pk>/', views.car_detail_view, name='car'),
-    
+    path('orders/', views.OrderListView.as_view(), name='orders'),
+    path('order/<int:pk>/', views.OrderDetailView.as_view(), name='order'),
     
 ]
--- ptu5_autoservice/autoservice/views.py
+++ ptu5_autoservice/autoservice/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from . import models
+from django.views import generic
 
 def index(request):
     return render(request, 'autoservice/index.html', {
@@ -13,3 +14,12 @@ def car_list_view(request):
 
 def car_detail_view(request, pk):
     return render(request, 'autoservice/car_detail.html', {'object': get_object_or_404(models.Car, pk=pk)})
+
+class OrderListView(generic.ListView):
+    model = models.Order
+    template_name = 'autoservice/order_list.html'
+
+
+class OrderDetailView(generic.DetailView):
+    model = models.Order
+    template_name = 'autoservice/order_detail.html'
